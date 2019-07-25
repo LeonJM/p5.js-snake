892af6e51d2b4632bccf8edf876d1321808607d4
Maleon Johan-Mosi <leon.jm@outlook.com>
2019-07-25 22:57:21 +1000

    5

--- script.js
+++ script.js
@@ -1,75 +1,3 @@
-<<<<<<< HEAD
-//LeonJM
-
-var w = 10; //width of cells
-var grid;
-var rows;
-var cols;
-
-var canvasWidth = 600;
-var canvasHeight = 600;
-
-var snakeBody = [];
-
-function makeDoubleArray(x, y) {
-	var arr = new Array(x);
-	for (var i = 0; i < arr.length; i ++) {
-		arr[i] = new Array(y);
-	}
-	return arr;
-}
-
-function setup() {
-	createCanvas(canvasWidth + 1, canvasHeight + 1);
-	frameRate(20);
-	background(0);
-	
-	rows = floor(width/w);
-	cols = floor(height/w);
-	grid = makeDoubleArray(rows,cols);
-	
-	
-	
-	snakeBody.push(new Cell(7,20,w,true));
-	
-}
-
-function draw() {
-	snakeBody.forEach();
-}
-
-show = function(x, y) {
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
 var canvasWidth;
 var canvasHeight;
 
@@ -142,5 +70,5 @@ function keyPressed() {
 		}
 	}
 }
->>>>>>> 498d989626da9edf418b7d534a4f469e805b3a95
+
 
